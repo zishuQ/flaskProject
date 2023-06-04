b7fef58002617e203aae79fa3f99c619a2d88121
zishuQ <1574908708@qq.com>
2023-06-04 17:56:29 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="static/img/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/home')
-@app.route('/index')
-def index():
+def home():
     return 'WelCome to My Flask'
 
 
@@ -36,6 +35,21 @@ def test_url_for():
     """
     return 'Test page'
 
+
+# 虚拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 if __name__ == '__main__':
     app.run()
